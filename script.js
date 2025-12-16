59a90ebd02d2c50fdbf7468239f10d4695f97bf3
dasarimaduri23 <dasarimaduri23@gmail.com>
2025-12-16 12:11:01 +0530

    for selecting and manipulating elements used dom

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // Replace with your OpenWeatherMap API key
 const API_KEY = "869a63cd732b74656ca32da705c36a53"; //place you api key 
 console.log("✅ JS file loaded!");
+
+// DOM elements
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const locBtn = document.getElementById("locBtn");
+const currentWeatherEl = document.getElementById("currentWeather");
+const forecastEl = document.getElementById("forecast");
+const errorBox = document.getElementById("errorBox");
+const recentWrapper = document.getElementById("recentWrapper");
+const recentDropdown = document.getElementById("recentDropdown");
+const unitToggle = document.getElementById("unitToggle");
